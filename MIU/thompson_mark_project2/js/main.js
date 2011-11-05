1c5eab4fb73e715eddf75335907b49f12dd3ac82
Mark Thompson <mark2thompson@gmail.com>
2011-11-04 22:02:57 -0400

    project 2 submit

--- MIU/thompson_mark_project2/js/main.js
+++ MIU/thompson_mark_project2/js/main.js
@@ -2,6 +2,8 @@
 // MIU 1111 Project 1
 // Javascript data functions
 //********************************************************************************
+
+//********************************************************************************
 function showRange(v){
 		document.getElementById('range').innerHTML=v;
 }
@@ -15,16 +17,16 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 //********************************************************************************
 //search
-/*var search = $('searchButton');
+/*var search = $('search');
 search.addEventListener("click", getSearch);
 
 	function getSearch(){
 		var category = $('dealType').value;
 		var term = $('search').value;
-	}
+	
 	
 //search by catagory
-if(catagory != "--Choose type of Deal--" && term === ""){
+if(category != "--Choose type of Deal--" && term === ""){
 	for(i=0, j=localStorage.length; i<j; i++){
 		var key = localStorage.key(i);
 		var value = localStorage.getItem(key);
@@ -59,13 +61,14 @@ if (term != "" && category != "--Choose type of Deal--"){
 		var value = localStorage.getItem(key);
 		var obj = JSON.parse(value);
 		for (n in obj){
-			if (term === obj[n][1] $$ category === obj.group[1]){
+			if (term === obj[n][1] && category === obj.group[1]){
 				for (q in obj){
 					alert(obj[q][1]);
 				}
 			}
 		}
 	}
+}
 }
 */
 //********************************************************************************
@@ -136,6 +139,7 @@ if (term != "" && category != "--Choose type of Deal--"){
 		var makeDiv = document.createElement('div');
 		makeDiv.setAttribute("id", "items");
 		var makeList = document.createElement('ul');
+		makeList.setAttribute("data-roll", "listview");
 		makeDiv.appendChild(makeList);
 		document.body.appendChild(makeDiv);
 		$('items').style.display = "block";
@@ -185,6 +189,168 @@ if (term != "" && category != "--Choose type of Deal--"){
 				"dealType": ["Type:", "Web Grooming"],
 				"dName": ["Deal Name:", "Mach3 razors"],
 				"url": ["URL:", "http://www.walmart.com"],
+				"rangeBar": ["Rating:", "9"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal3": {
+				"dealType": ["Type:", "Store Grooming"],
+				"dName": ["Deal Name:", "Mach3 razors"],
+				"url": ["URL:", "http://www.walmart.com"],
+				"rangeBar": ["Rating:", "9"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal4": {
+				"dealType": ["Type:", "Store Auto"],
+				"dName": ["Deal Name:", "Oil Change"],
+				"url": ["URL:", "http://www.autozone.com"],
+				"rangeBar": ["Rating:", "6"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal5": {
+				"dealType": ["Type:", "Store Entertainment"],
+				"dName": ["Deal Name:", "BTTF boxset"],
+				"url": ["URL:", "http://www.walmart.com"],
+				"rangeBar": ["Rating:", "10"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal6": {
+				"dealType": ["Type:", "Web Entertainment"],
+				"dName": ["Deal Name:", "Free Movie Tickets"],
+				"url": ["URL:", "http://www.fandango.com"],
+				"rangeBar": ["Rating:", "10"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal7": {
+				"dealType": ["Type:", "Web Auto"],
+				"dName": ["Deal Name:", "Spark Plugs"],
+				"url": ["URL:", "http://www.summitracing.com"],
+				"rangeBar": ["Rating:", "5"],
+				"favoriteDeal": ["Fav deal:", "No"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal8": {
+				"dealType": ["Type:", "Web Grooming"],
+				"dName": ["Deal Name:", "axe soap"],
+				"url": ["URL:", "http://www.target.com"],
+				"rangeBar": ["Rating:", "8"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal9": {
+				"dealType": ["Type:", "Store Grooming"],
+				"dName": ["Deal Name:", "BOGO Mach3 razors"],
+				"url": ["URL:", "http://www.publix.com"],
+				"rangeBar": ["Rating:", "10"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal10": {
+				"dealType": ["Type:", "Web Entertainment"],
+				"dName": ["Deal Name:", "itunes free song"],
+				"url": ["URL:", "http://www.apple.com/itunes"],
+				"rangeBar": ["Rating:", "9"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal11": {
+				"dealType": ["Type:", "Store Entertainment"],
+				"dName": ["Deal Name:", "Park AV CD recording - free poster"],
+				"url": ["URL:", "http://www.parkavecd.com"],
+				"rangeBar": ["Rating:", "7"],
+				"favoriteDeal": ["Fav deal:", "No"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal12": {
+				"dealType": ["Type:", "Store Grooming"],
+				"dName": ["Deal Name:", "BOGO TP"],
+				"url": ["URL:", "http://www.publix.com"],
+				"rangeBar": ["Rating:", "8"],
+				"favoriteDeal": ["Fav deal:", "No"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal13": {
+				"dealType": ["Type:", "Web Entertainment"],
+				"dName": ["Deal Name:", "1 month free netflix"],
+				"url": ["URL:", "http://www.netflix.com"],
+				"rangeBar": ["Rating:", "5"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal14": {
+				"dealType": ["Type:", "Web Auto"],
+				"dName": ["Deal Name:", "cheap gas cards"],
+				"url": ["URL:", "http://www.fakecheapgascards.com"],
+				"rangeBar": ["Rating:", "3"],
+				"favoriteDeal": ["Fav deal:", "No"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal15": {
+				"dealType": ["Type:", "Web Auto"],
+				"dName": ["Deal Name:", "car carpet cleaner"],
+				"url": ["URL:", "http://www.awesomecarpetcleaner.com"],
+				"rangeBar": ["Rating:", "9"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal16": {
+				"dealType": ["Type:", "Web Grooming"],
+				"dName": ["Deal Name:", "Mach3 razors"],
+				"url": ["URL:", "http://www.walmart.com"],
+				"rangeBar": ["Rating:", "9"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal17": {
+				"dealType": ["Type:", "Web Entertainment"],
+				"dName": ["Deal Name:", "iTunes gift cards"],
+				"url": ["URL:", "http://www.bestbuy.com"],
+				"rangeBar": ["Rating:", "6"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal18": {
+				"dealType": ["Type:", "Web Entertainment"],
+				"dName": ["Deal Name:", "Steve Jobs Book"],
+				"url": ["URL:", "http://www.amazon.com"],
+				"rangeBar": ["Rating:", "10"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal19": {
+				"dealType": ["Type:", "Store Grooming"],
+				"dName": ["Deal Name:", "BOGO shaving cream"],
+				"url": ["URL:", "http://www.publix.com"],
+				"rangeBar": ["Rating:", "7"],
+				"favoriteDeal": ["Fav deal:", "Yes"],
+				"exDate": ["Exp Date:", "2012/10/15"],
+				"notes": ["Notes:", "These are just some test notes."]			
+			},
+			"deal20": {
+				"dealType": ["Type:", "Store Entertainment"],
+				"dName": ["Deal Name:", "BOGO Halloween DVDs"],
+				"url": ["URL:", "http://www.bestbuy.com"],
 				"rangeBar": ["Rating:", "9"],
 				"favoriteDeal": ["Fav deal:", "Yes"],
 				"exDate": ["Exp Date:", "2012/10/15"],
