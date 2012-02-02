7bfbc862650a64ec7f46c7d05dc192b8bac82a8c
Mark Thompson <mark2thompson@gmail.com>
2012-02-01 21:10:22 -0500

    added autoFillData function for json file, removed json from main js file,

--- ASD/project1/js/main.js
+++ ASD/project1/js/main.js
@@ -132,7 +132,7 @@ if (term != "" && category != "--Choose type of Deal--"){
 	function getData(){
 		toggleControls("on");
 		if(localStorage.length === 0){
-			autoFillData();
+			autoFillData(defaultsJson);
 			alert("There is no deals in Local Storage so default deals were added.");
 		}
 		var makeDiv = $("<div></div>");
@@ -149,15 +149,17 @@ if (term != "" && category != "--Choose type of Deal--"){
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			var obj = JSON.parse(value);
+			console.log (obj)
 			var makeSubList = $('<ul></ul>');
 			makeLi.append(makeSubList);
-			//add image for group
+			
 			getImage(obj.dealType[1], makeSubList);
 			for(var n in obj){
+				var obj = JSON.parse(value);
 				var makeSubLi = $('<li></li>');
 				makeSubList.append(makeSubLi);
 				var optSubText = obj[n][0]+" "+obj[n][1];
-				makeSubLi.innerHTML = optSubText;	
+				makeSubLi.html (optSubText);	
 				makeSubList.append(linksLi);
 			}
 			makeItemLinks(localStorage.key(i), linksLi); // creats edit and delete buttons for local storage. 
@@ -173,189 +175,7 @@ if (term != "" && category != "--Choose type of Deal--"){
 	}
 //**************************THIS IS JUST FOR TESTING******************************
 	//JSON OBJECT... auto populate local storage. 
-	function autoFillData(){
-		var JSON = {
-			"deal1": {
-				"dealType": ["Type:", "Web Auto"],
-				"dName": ["Deal Name:", "Oil Change"],
-				"url": ["URL:", "http://www.autozone.com"],
-				"rangeBar": ["Rating:", "7"],
-				"favoriteDeal": ["Fav deal:", "No"],
-				"exDate": ["Exp Date:", "2012/10/09"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal2": {
-				"dealType": ["Type:", "Web Grooming"],
-				"dName": ["Deal Name:", "Mach3 razors"],
-				"url": ["URL:", "http://www.walmart.com"],
-				"rangeBar": ["Rating:", "9"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal3": {
-				"dealType": ["Type:", "Store Grooming"],
-				"dName": ["Deal Name:", "Mach3 razors"],
-				"url": ["URL:", "http://www.walmart.com"],
-				"rangeBar": ["Rating:", "9"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal4": {
-				"dealType": ["Type:", "Store Auto"],
-				"dName": ["Deal Name:", "Oil Change"],
-				"url": ["URL:", "http://www.autozone.com"],
-				"rangeBar": ["Rating:", "6"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal5": {
-				"dealType": ["Type:", "Store Entertainment"],
-				"dName": ["Deal Name:", "BTTF boxset"],
-				"url": ["URL:", "http://www.walmart.com"],
-				"rangeBar": ["Rating:", "10"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal6": {
-				"dealType": ["Type:", "Web Entertainment"],
-				"dName": ["Deal Name:", "Free Movie Tickets"],
-				"url": ["URL:", "http://www.fandango.com"],
-				"rangeBar": ["Rating:", "10"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal7": {
-				"dealType": ["Type:", "Web Auto"],
-				"dName": ["Deal Name:", "Spark Plugs"],
-				"url": ["URL:", "http://www.summitracing.com"],
-				"rangeBar": ["Rating:", "5"],
-				"favoriteDeal": ["Fav deal:", "No"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal8": {
-				"dealType": ["Type:", "Web Grooming"],
-				"dName": ["Deal Name:", "axe soap"],
-				"url": ["URL:", "http://www.target.com"],
-				"rangeBar": ["Rating:", "8"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal9": {
-				"dealType": ["Type:", "Store Grooming"],
-				"dName": ["Deal Name:", "BOGO Mach3 razors"],
-				"url": ["URL:", "http://www.publix.com"],
-				"rangeBar": ["Rating:", "10"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal10": {
-				"dealType": ["Type:", "Web Entertainment"],
-				"dName": ["Deal Name:", "itunes free song"],
-				"url": ["URL:", "http://www.apple.com/itunes"],
-				"rangeBar": ["Rating:", "9"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal11": {
-				"dealType": ["Type:", "Store Entertainment"],
-				"dName": ["Deal Name:", "Park AV CD recording - free poster"],
-				"url": ["URL:", "http://www.parkavecd.com"],
-				"rangeBar": ["Rating:", "7"],
-				"favoriteDeal": ["Fav deal:", "No"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal12": {
-				"dealType": ["Type:", "Store Grooming"],
-				"dName": ["Deal Name:", "BOGO TP"],
-				"url": ["URL:", "http://www.publix.com"],
-				"rangeBar": ["Rating:", "8"],
-				"favoriteDeal": ["Fav deal:", "No"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal13": {
-				"dealType": ["Type:", "Web Entertainment"],
-				"dName": ["Deal Name:", "1 month free netflix"],
-				"url": ["URL:", "http://www.netflix.com"],
-				"rangeBar": ["Rating:", "5"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal14": {
-				"dealType": ["Type:", "Web Auto"],
-				"dName": ["Deal Name:", "cheap gas cards"],
-				"url": ["URL:", "http://www.fakecheapgascards.com"],
-				"rangeBar": ["Rating:", "3"],
-				"favoriteDeal": ["Fav deal:", "No"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal15": {
-				"dealType": ["Type:", "Web Auto"],
-				"dName": ["Deal Name:", "car carpet cleaner"],
-				"url": ["URL:", "http://www.awesomecarpetcleaner.com"],
-				"rangeBar": ["Rating:", "9"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal16": {
-				"dealType": ["Type:", "Web Grooming"],
-				"dName": ["Deal Name:", "Mach3 razors"],
-				"url": ["URL:", "http://www.walmart.com"],
-				"rangeBar": ["Rating:", "9"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal17": {
-				"dealType": ["Type:", "Web Entertainment"],
-				"dName": ["Deal Name:", "iTunes gift cards"],
-				"url": ["URL:", "http://www.bestbuy.com"],
-				"rangeBar": ["Rating:", "6"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal18": {
-				"dealType": ["Type:", "Web Entertainment"],
-				"dName": ["Deal Name:", "Steve Jobs Book"],
-				"url": ["URL:", "http://www.amazon.com"],
-				"rangeBar": ["Rating:", "10"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal19": {
-				"dealType": ["Type:", "Store Grooming"],
-				"dName": ["Deal Name:", "BOGO shaving cream"],
-				"url": ["URL:", "http://www.publix.com"],
-				"rangeBar": ["Rating:", "7"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			},
-			"deal20": {
-				"dealType": ["Type:", "Store Entertainment"],
-				"dName": ["Deal Name:", "BOGO Halloween DVDs"],
-				"url": ["URL:", "http://www.bestbuy.com"],
-				"rangeBar": ["Rating:", "9"],
-				"favoriteDeal": ["Fav deal:", "Yes"],
-				"exDate": ["Exp Date:", "2012/10/15"],
-				"notes": ["Notes:", "These are just some test notes."]			
-			}	
-		};
+	function autoFillData(json){
 		//store the json obj into local storage
 		for(var n in json){
 			var id = Math.floor(Math.random()*100000001);
