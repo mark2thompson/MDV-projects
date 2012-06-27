23271b73a79809ef50908dff2981c557fa68f598
Mark Thompson <mark2thompson@gmail.com>
2012-06-27 10:46:57 -0400

    git commit to confirm commits in xcode

---
+++ AOC1/appTest/appTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  appTest
+//
+//  Created by Mark Thompson on 6/26/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC1/appTest/appTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  appTest
+//
+//  Created by Mark Thompson on 6/26/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
