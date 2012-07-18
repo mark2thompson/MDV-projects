b79ffac4e5c93befb30ff53753538efa1df74f45
Mark Thompson <mark2thompson@gmail.com>
2012-07-18 19:01:45 -0400

    added comments to my viewController to start the project. in the assignment it says to put your code in viewDidAppear, I do not have that function on my new file. I checked and i believe the 2 are similar enough for this project to still work correctly, will add further comments if I'm wrong

---
+++ AOC1/appTest3/appTest3/ViewController.m
@@ -0,0 +1,55 @@
+//
+//  ViewController.m
+//  appTest3
+//
+//  Created by Mark Thompson on 7/18/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two.
+    
+    //Create a function called Compare that takes two NSInteger values. Return true or false based on whether the values are equal.
+    
+    //Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
+    
+    //Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+    
+    //Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
+    
+    //Call the Add function passing in two integer values. Capture the return of this function into a variable.
+    
+    //Bundle the returned integer into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+    
+    //Give it some text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
+    
+    //Call the Compare function with two integer values. If Compare returns true, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function
+    
+    
+
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
