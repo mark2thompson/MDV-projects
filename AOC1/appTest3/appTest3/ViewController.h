c19f5a3e27b7c4f1092819a62b44bb73e7386c02
Mark Thompson <mark2thompson@gmail.com>
2012-07-20 18:38:34 -0400

    added my add function to my viewController.h file

---
+++ AOC1/appTest3/appTest3/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  appTest3
+//
+//  Created by Mark Thompson on 7/18/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+
+}
+- (NSNumber *)Add:(NSInteger)firstNum  addNum:(NSInteger)secondNum;
+
+
+
+@end
