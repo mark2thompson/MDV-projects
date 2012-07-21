3248391b3a5570ce0cdb2f71486f36a0655544c9
Mark Thompson <mark2thompson@gmail.com>
2012-07-20 23:32:31 -0400

    final commit in git for sync

---
+++ AOC1/appTest3/appTest3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  appTest3
+//
+//  Created by Mark Thompson on 7/18/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC1/appTest3/appTest3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  appTest3
+//
+//  Created by Mark Thompson on 7/18/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
