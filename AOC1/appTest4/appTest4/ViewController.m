c0d623987b7e37e335c6884dbf5219464e146640
Mark Thompson <mark2thompson@gmail.com>
2012-07-26 18:24:26 -0400

    added nsDate  and displayed alert view with date and time

--- AOC1/appTest4/appTest4/ViewController.m
+++ AOC1/appTest4/appTest4/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #define button_zero 0
+#define button_one 1
 
 @interface ViewController ()
 
@@ -50,12 +51,24 @@
         unDialog.textAlignment = UITextAlignmentCenter;
         unDialog.backgroundColor = [UIColor grayColor];
         unDialog.textColor = [UIColor whiteColor];
-    };    
-    
+    };       
+    //Create a UIButton using the rounded rectangle type. Give this button any color you wish.
+    UIButton *dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (dateButton != nil)
+    {
+        dateButton.frame = CGRectMake(230.0f, 0, 100.0f, 30.0f);
+        //Add the text "Show Date" to the button
+        [dateButton setTitle:@"Show Date" forState:UIControlStateNormal];
+        //Add an action to the button that when clicked, it will call the same onClick handler you already defined. Make sure to add a tag to the date button so you know which one was pressed.
+        dateButton.tag = button_one;
+        [dateButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+    };
+
     [self.view addSubview:userName]; 
     [self.view addSubview:userField];
     [self.view addSubview:loginButton];
     [self.view addSubview:unDialog];
+    [self.view addSubview:dateButton];
     
 }
 //onClickfunction
@@ -65,19 +78,38 @@
     if (loginButton.tag == button_zero)
     {
      //If the user has not entered any text into the UITextField, display in the UILabel, "Username cannot be empty". Otherwise, display "User: username has been logged in".   
-     if (userField.text.length > 0)
-     {
-         [unDialog resignFirstResponder];
-         NSString *logged = [NSString stringWithFormat:@"User: %@ has been logged in", userField.text, nil];
-         unDialog.text = logged;
-     }
-     else {
-         [unDialog resignFirstResponder];
-         unDialog.text = @"Username cannot be empty";
-     }
-        
+        if (userField.text.length > 0)
+            {
+                [unDialog resignFirstResponder];
+                NSString *logged = [NSString stringWithFormat:@"User: %@ has been logged in", userField.text, nil];
+                unDialog.text = logged;
+            }
+        else 
+            {
+            [unDialog resignFirstResponder];
+            unDialog.text = @"Username cannot be empty";
+            }
     }
-}
+    else if(loginButton.tag == button_one)
+    {
+        //Display a UIAlertView with the current date and time displayed in the format seen in the dateAlert graphic in the assets section of this project assignment. You can either format the date and time manually or use the date and time styles. You must use an NSDate object to gather the date and time information.
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"HH:mm MMMM dd, yyyy"];
+        
+        NSDate *date = [NSDate date];
+        
+        NSString *formattedDateString = [dateFormatter stringFromDate:date];
+        NSLog(@"formattedDateString: %@", formattedDateString);
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Date" message:(@"formattedDateString: %@", formattedDateString) delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+        if (alertView != nil)
+        {
+            [alertView show];
+        }
+       
+    }
+};
 
 - (void)viewDidUnload
 {
