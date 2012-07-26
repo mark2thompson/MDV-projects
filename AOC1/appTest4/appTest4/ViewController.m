014d204bcd7f8dbc5ce604dbb286dd8b74ee6fc3
Mark Thompson <mark2thompson@gmail.com>
2012-07-26 10:42:09 -0400

    created UILabel

--- AOC1/appTest4/appTest4/ViewController.m
+++ AOC1/appTest4/appTest4/ViewController.m
@@ -20,16 +20,21 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor whiteColor];
     //Create a UILabel near the top of your screen with teh text "Username:" in it.
-    UILabel *userName = [[UILabel alloc]initWithFrame:CGRectMake(0, 10.0f, 320.0f, 20.0f)];
+    UILabel *userName = [[UILabel alloc]initWithFrame:CGRectMake(0, 15.0f, 100.0f, 20.0f)];
     if (userName != nil)
     {
         userName.text = @"Username: ";
     };
+    //Create a UITextField to the right of the username label
+    UITextField *userField = [[UITextField alloc]initWithFrame:CGRectMake(90.0f, 10.0f, 220.0f, 30.0f)];
+    if (userField != nil)
+        {
+            userField.borderStyle = UITextBorderStyleRoundedRect;
+        }
     
     
-    
-    
-   [self.view addSubview:userName]; 
+    [self.view addSubview:userName]; 
+    [self.view addSubview:userField];
     
 }
 
