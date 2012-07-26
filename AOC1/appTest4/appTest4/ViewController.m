c163aab9adf03527acafb2ddb51ec881e838f1e0
Mark Thompson <mark2thompson@gmail.com>
2012-07-26 17:43:45 -0400

    created : If the user has not entered any text into the UITextField, display in the UILabel, "Username cannot be empty". Otherwise, display "User: username has been logged in".

--- AOC1/appTest4/appTest4/ViewController.m
+++ AOC1/appTest4/appTest4/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#define button_zero 0
 
 @interface ViewController ()
 
@@ -26,7 +27,7 @@
         userName.text = @"Username: ";
     };
     //Create a UITextField to the right of the username label
-    UITextField *userField = [[UITextField alloc]initWithFrame:CGRectMake(90.0f, 10.0f, 220.0f, 30.0f)];
+    userField = [[UITextField alloc]initWithFrame:CGRectMake(90.0f, 10.0f, 220.0f, 30.0f)];
     if (userField != nil)
     {
         userField.borderStyle = UITextBorderStyleRoundedRect;
@@ -37,24 +38,45 @@
     {
         loginButton.frame = CGRectMake(210.0f, 60.0f, 100.0f, 30.0f);
         [loginButton setTitle:@"Login" forState:UIControlStateNormal];
-        
+        //Add a target to the UIButton to call a function called onClick when the user presses the Login button.
+        loginButton.tag = button_zero;
+        [loginButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
     };
     //Create another UILabel beneath with the default text "Please Enter Username".
-    UILabel *unDialog = [[UILabel alloc]initWithFrame:CGRectMake(0, 120.0f, 320.0f, 60.0f)];
+    unDialog = [[UILabel alloc]initWithFrame:CGRectMake(0, 120.0f, 320.0f, 60.0f)];
     if (unDialog != nil)
     {
         unDialog.text = @"Please Enter Username";
         unDialog.textAlignment = UITextAlignmentCenter;
         unDialog.backgroundColor = [UIColor grayColor];
         unDialog.textColor = [UIColor whiteColor];
-    };
-    
+    };    
     
     [self.view addSubview:userName]; 
     [self.view addSubview:userField];
     [self.view addSubview:loginButton];
     [self.view addSubview:unDialog];
     
+}
+//onClickfunction
+
+-(void)onClick:(UIButton*)loginButton
+{
+    if (loginButton.tag == button_zero)
+    {
+     //If the user has not entered any text into the UITextField, display in the UILabel, "Username cannot be empty". Otherwise, display "User: username has been logged in".   
+     if (userField.text.length > 0)
+     {
+         [unDialog resignFirstResponder];
+         NSString *logged = [NSString stringWithFormat:@"User: %@ has been logged in", userField.text, nil];
+         unDialog.text = logged;
+     }
+     else {
+         [unDialog resignFirstResponder];
+         unDialog.text = @"Username cannot be empty";
+     }
+        
+    }
 }
 
 - (void)viewDidUnload
