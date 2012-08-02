f58bc1e5e64345b78d81fbe20a29a11adb027ab7
Mark Thompson <mark2thompson@gmail.com>
2012-08-02 14:25:26 -0400

    Added first class

---
+++ AOCII/objcIIProject1/objcIIProject1/appleStuff.h
@@ -0,0 +1,13 @@
+//
+//  appleStuff.h
+//  objcIIProject1
+//
+//  Created by Mark Thompson on 8/2/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface appleStuff : NSObject
+
+@end
---
+++ AOCII/objcIIProject1/objcIIProject1/appleStuff.m
@@ -0,0 +1,13 @@
+//
+//  appleStuff.m
+//  objcIIProject1
+//
+//  Created by Mark Thompson on 8/2/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import "appleStuff.h"
+
+@implementation appleStuff
+
+@end
