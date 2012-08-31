61bfe03d6daa1ad0fe013f666c6b6d701661b7fd
Mark Thompson <mark2thompson@gmail.com>
2012-08-30 21:55:12 -0400

    commit

---
+++ AOCII/objc2Project1/objc2Project1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  objc2Project1
+//
+//  Created by Mark Thompson on 8/2/12.
+//  Copyright (c) 2012 markthompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
