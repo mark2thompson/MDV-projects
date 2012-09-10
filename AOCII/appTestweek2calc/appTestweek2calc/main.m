d0e012c4ae5ba0a515720205e17cd5a4e1905c46
Mark Thompson <mark2thompson@gmail.com>
2012-09-09 23:42:45 -0400

    final commit

---
+++ AOCII/appTestweek2calc/appTestweek2calc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  appTestweek2calc
+//
+//  Created by Mark Thompson on 9/8/12.
+//  Copyright (c) 2012 Mark Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
