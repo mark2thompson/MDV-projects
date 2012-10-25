5ec2ab151917ce44b82fca1cff0a68b9381f1264
Mark Thompson <mark2thompson@gmail.com>
2012-10-25 18:20:01 -0400

    created bait fishing rig and calculated time - following allong with video

--- AOCII/project1/project1/ViewController.h
+++ AOCII/project1/project1/ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "fishingFactory.h"
 
 @interface ViewController : UIViewController
 
--- AOCII/project1/project1/ViewController.m
+++ AOCII/project1/project1/ViewController.m
@@ -16,6 +16,34 @@
 
 - (void)viewDidLoad
 {
+    //create a bait fishing rig and set bait size
+    baitFishing *baitFishingRig = (baitFishing*)[fishingFactory
+                                                 createFishingTrip:BAIT];
+    [baitFishingRig setSize:2];
+    
+    if (baitFishingRig !=nil){
+        
+        NSArray *baitRigItems = [[NSArray alloc]
+            initWithObjects:@"Hook", @"Shrimp", nil];
+        [baitFishingRig setItemsNeeded:baitRigItems];
+        
+        NSString *baitRigInstructions = @"Hook Shrimp through the tail and cast to desired location.";
+        [baitFishingRig setInstructions:baitRigInstructions];
+        
+        NSLog(@"You are now fishing with a bait rig! "
+              "with the items %@", [baitFishingRig itemsNeeded]);
+        NSLog(@"%@", baitFishingRig.instructions);
+        
+        // calculate fishing time
+        [baitFishingRig calculateFishingTime];
+    }
+    
+    //set instructions and size of bait and output
+    //calculate and log fishing time
+    
+    // create a lure fishing rig and set lure type and name
+    // calculate and log the fishing time
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
